00cf403820f21fbbaf7a7159747cb35c0bc05054
BoongaBonga <timo.vandeneynde@leerling.kov.be>
2024-09-14 13:46:18 +0200

    9th commit

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const delay = 50;
 const purpose_scale = document.getElementById("purpose_scale");
 const Idle_reflection = document.createElement("button");
 Idle_reflection.textContent = "Idle reflection";
+const idleReflectionPriceDisplay = document.createElement("p");
+idleReflectionPriceDisplay.textContent = "Buy for <span id = 'idleReflectionPriceID'>10</span> purpose (0.5 purpose/s)";
 
 function gainPurpose(gain){
     //increment purpose counter
@@ -19,7 +21,8 @@ function buyIdleReflection(count){
     if(purpose >= idleReflectionPrice){
         purpose = purpose - idleReflectionPrice;
         idleReflectionCount = idleReflectionCount + count;
-        idleReflectionPrice = idleReflectionPrice * 1.3;
+        idleReflectionPrice = Math.round(idleReflectionPrice * 1.3);
+        document.getElementById("idleReflectionPriceID").innerHTML = idleReflectionPrice;
     } else{
         Idle_reflection.innerHTML = "Not enough purpose";
         setTimeout(1000);
@@ -28,8 +31,8 @@ function buyIdleReflection(count){
 }
 
 window.setInterval(function(){
-    purpose = purpose + idleReflectionCount * (delay/1000);
-    document.getElementById("purpose").innerHTML = purpose;
+    purpose = purpose + idleReflectionCount * (delay/2000);
+    document.getElementById("purpose").innerHTML = Math.round(purpose);
 
     //change scale based on purpose
     if(purpose >= 10 && purpose < 50 && operationNr === 0){
@@ -42,7 +45,7 @@ window.setInterval(function(){
         purpose_scale.innerHTML = "A bird briefly lands near you before flying away.";
         operationNr = 2;
     }
-    if(purpose >= 100 && purpose < 500 && operationNr === 3){
+    if(purpose >= 100 && purpose < 500 && operationNr === 2){
         purpose_scale.innerHTML = "A stray cat brushes against your leg.";
         operationNr = 3;
     }
