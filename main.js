00fb33c5d26ee921083904514b17bda3a6c56b0c
BoongaBonga <timo.vandeneynde@leerling.kov.be>
2024-09-14 08:57:13 +0200

    4th commit

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 let purpose = 0;
+
 function NGU(){
     purpose = purpose + 1;
     document.getElementById("purpose").innerHTML = purpose;
+}
+
+const purpose_scale = document.getElementById("purpose_scale");
+
+if(purpose >= 10 && purpose < 50){
+    purpose_scale.innerHTML = "A flea notices you and jumps an your hair.";
+} else if(purpose >= 50 && purpose < 100){
+    purpose_scale.innerHTML = "A stray cat brushes against your leg.";
+} else if(purpose >= 100 && purpose < 500){
+    purpose_scale.innerHTML = "A bird briefly lands near you before flying away.";
 }
