478a60a2a538edbb342b894fbbb38c1979b823f7
BoongaBonga <timo.vandeneynde@leerling.kov.be>
2024-09-14 11:19:21 +0200

    6th commit

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 let purpose = 0;
 const purpose_scale = document.getElementById("purpose_scale");
+const Idle_reflection = document.createElement("button");
 
-function NGU(){
+function gainPurpose(){
     //increment purpose counter
     purpose = purpose + 1;
     document.getElementById("purpose").innerHTML = purpose;
     //change scale based on purpose
     if(purpose >= 10 && purpose < 50){
-        purpose_scale.innerHTML = "A flea notices you and jumps an your hair.";
+        purpose_scale.innerHTML = "A flea notices you and jumps onto your hair.";
+        document.getElementById("Idle_reflection").appendChild(Idle_reflection);
     }
     if(purpose >= 50 && purpose < 100){
-        purpose_scale.innerHTML = "A stray cat brushes against your leg.";
+        purpose_scale.innerHTML = "A bird briefly lands near you before flying away.";
     }
     if(purpose >= 100 && purpose < 500){
-        purpose_scale.innerHTML = "A bird briefly lands near you before flying away.";
+        purpose_scale.innerHTML = "A stray cat brushes against your leg.";
     }
 }
 
